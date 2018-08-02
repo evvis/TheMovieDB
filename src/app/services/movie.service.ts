6545afbfc71989817c00ab429ef4f97f0bcadce4
Evgeniy Viskunov <evgeniy.viskunov@live.com>
2018-08-02 22:16:50 +0700

    cleaning

--- src/app/services/movie.service.ts
+++ src/app/services/movie.service.ts
@@ -1,7 +1,6 @@
 import {Injectable} from '@angular/core';
 import {Http, Response} from '@angular/http';
 import {Observable} from 'rxjs/Observable';
-//import {Movie} from './movie';
 import 'rxjs/add/operator/map';
 
 @Injectable()
