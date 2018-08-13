f4bfe9a7e3810324d22466dbd1966b0cf9b7a92e
Evgeniy Viskunov <evgeniy.viskunov@live.com>
2018-08-13 16:29:05 +0700

    Merge from details-component, generate movie-recommendations component

---
+++ src/app/movie-recommendations/movie-recommendations.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-movie-recommendations',
+  templateUrl: './movie-recommendations.component.html',
+  styleUrls: ['./movie-recommendations.component.css']
+})
+export class MovieRecommendationsComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
